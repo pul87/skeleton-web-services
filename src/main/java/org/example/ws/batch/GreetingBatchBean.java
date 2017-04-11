ed5b2760e275820af1db4fa4d28466e6357dd7de
Paolo Basso <paolo.basso87@gmail.com>
2017-04-11 21:30:17 +0200

    Spring Profiles

--- src/main/java/org/example/ws/batch/GreetingBatchBean.java
+++ src/main/java/org/example/ws/batch/GreetingBatchBean.java
@@ -7,9 +7,11 @@ import org.example.ws.service.GreetingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Profile("batch")
 @Component
 public class GreetingBatchBean {
 
@@ -19,8 +21,8 @@ public class GreetingBatchBean {
 	private GreetingService greetingService;
 	
 	// Gira ad ogni "0" e "30" di ogni minuto di ogni ora di ogni giorno di ogni mese di ogni anno.
-	//@Scheduled(cron="0,30 * * * * *")
-	public void chronJob() {
+	@Scheduled(cron="${batch.greeting.cron}")
+	public void cronJob() {
 		logger.info("> chronJob ogni 0 e 30 secondi di ogni minuto.");
 		
 		// Scheduled logic here
@@ -31,17 +33,17 @@ public class GreetingBatchBean {
 		logger.info("< chronJob");
 	}
 	
-	/*@Scheduled( 
-			initialDelay=5000,
-			fixedRate=5000 )*/
+	@Scheduled( 
+			initialDelayString="${batch.greeting.initialdelay}",
+			fixedRateString="${batch.greeting.fixedrate}" )
 	public void fixedRateWithInitialDelay() {
 		logger.info("> chronJob ogni 5 secondi, dopo 5 secondi dalla partenza.");
 	}
 	
 	// Viene eseguito dopo un fixedDelay dall'esecuzione precedente
-	/*@Scheduled( 
-			initialDelay=2000,
-			fixedDelay=10000 )*/
+	@Scheduled( 
+			initialDelayString="${batch.greeting.initialdelay}",
+			fixedDelayString="${batch.greeting.fixeddelay}" )
 	public void fixedDelayJob() {
 		logger.info("> chronJob ogni 10 secondi dopo il termine dell'esecizione precedente, dopo 2 secondi dalla partenza.");
 	}
